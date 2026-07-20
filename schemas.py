4f10fc14bf6f9025ea07eefdf2c6645d45a77c42
Sami <sami.rehman1073@gmail.com>
2026-07-20 18:10:45 +0500

    Updated schemas.py for smooth conversion into pydantic object

--- schemas.py
+++ schemas.py
@@ -1,3 +1,6 @@
+from typing import Annotated
+
+from fastapi import Form
 from pydantic import BaseModel
 
 
@@ -47,3 +50,70 @@ class PredictionInput(BaseModel):
     concave_points_worst: float
     symmetry_worst: float
     fractal_dimension_worst: float
+
+    @classmethod
+    def as_form(
+        cls,
+        radius_mean: Annotated[float, Form(...)],
+        texture_mean: Annotated[float, Form(...)],
+        perimeter_mean: Annotated[float, Form(...)],
+        area_mean: Annotated[float, Form(...)],
+        smoothness_mean: Annotated[float, Form(...)],
+        compactness_mean: Annotated[float, Form(...)],
+        concavity_mean: Annotated[float, Form(...)],
+        concave_points_mean: Annotated[float, Form(...)],
+        symmetry_mean: Annotated[float, Form(...)],
+        fractal_dimension_mean: Annotated[float, Form(...)],
+        radius_se: Annotated[float, Form(...)],
+        texture_se: Annotated[float, Form(...)],
+        perimeter_se: Annotated[float, Form(...)],
+        area_se: Annotated[float, Form(...)],
+        smoothness_se: Annotated[float, Form(...)],
+        compactness_se: Annotated[float, Form(...)],
+        concavity_se: Annotated[float, Form(...)],
+        concave_points_se: Annotated[float, Form(...)],
+        symmetry_se: Annotated[float, Form(...)],
+        fractal_dimension_se: Annotated[float, Form(...)],
+        radius_worst: Annotated[float, Form(...)],
+        texture_worst: Annotated[float, Form(...)],
+        perimeter_worst: Annotated[float, Form(...)],
+        area_worst: Annotated[float, Form(...)],
+        smoothness_worst: Annotated[float, Form(...)],
+        compactness_worst: Annotated[float, Form(...)],
+        concavity_worst: Annotated[float, Form(...)],
+        concave_points_worst: Annotated[float, Form(...)],
+        symmetry_worst: Annotated[float, Form(...)],
+        fractal_dimension_worst: Annotated[float, Form(...)]
+    ):
+        return cls(
+            radius_mean=radius_mean,
+            texture_mean=texture_mean,
+            perimeter_mean=perimeter_mean,
+            area_mean=area_mean,
+            smoothness_mean=smoothness_mean,
+            compactness_mean=compactness_mean,
+            concavity_mean=concavity_mean,
+            concave_points_mean=concave_points_mean,
+            symmetry_mean=symmetry_mean,
+            fractal_dimension_mean=fractal_dimension_mean,
+            radius_se=radius_se,
+            texture_se=texture_se,
+            perimeter_se=perimeter_se,
+            area_se=area_se,
+            smoothness_se=smoothness_se,
+            compactness_se=compactness_se,
+            concavity_se=concavity_se,
+            concave_points_se=concave_points_se,
+            symmetry_se=symmetry_se,
+            fractal_dimension_se=fractal_dimension_se,
+            radius_worst=radius_worst,
+            texture_worst=texture_worst,
+            perimeter_worst=perimeter_worst,
+            area_worst=area_worst,
+            smoothness_worst=smoothness_worst,
+            compactness_worst=compactness_worst,
+            concavity_worst=concavity_worst,
+            concave_points_worst=concave_points_worst,
+            symmetry_worst=symmetry_worst,
+            fractal_dimension_worst=fractal_dimension_worst,
+        )
