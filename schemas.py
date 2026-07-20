b441cfca2e7e87745c3c2a44e1ef8a9e1c7153f7
Sami <sami.rehman1073@gmail.com>
2026-07-20 17:58:49 +0500

    Added schemas.py file

---
+++ schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+
+class PredictionInput(BaseModel):
+
+    radius_mean: float
+    texture_mean: float
+    perimeter_mean: float
+    area_mean: float
+    smoothness_mean: float
+    compactness_mean: float
+    concavity_mean: float
+    concave_points_mean: float
+    symmetry_mean: float
+    fractal_dimension_mean: float
