22b12cc476d135aad604b63c4444139a1f7a30f7
Sami <sami.rehman1073@gmail.com>
2026-07-20 18:04:55 +0500

    updated schemas.py with all 30 features

--- schemas.py
+++ schemas.py
@@ -3,6 +3,10 @@ from pydantic import BaseModel
 
 class PredictionInput(BaseModel):
 
+    # ==========================
+    # Mean Features
+    # ==========================
+
     radius_mean: float
     texture_mean: float
     perimeter_mean: float
@@ -13,3 +17,33 @@ class PredictionInput(BaseModel):
     concave_points_mean: float
     symmetry_mean: float
     fractal_dimension_mean: float
+
+    # ==========================
+    # Standard Error Features
+    # ==========================
+
+    radius_se: float
+    texture_se: float
+    perimeter_se: float
+    area_se: float
+    smoothness_se: float
+    compactness_se: float
+    concavity_se: float
+    concave_points_se: float
+    symmetry_se: float
+    fractal_dimension_se: float
+
+    # ==========================
+    # Worst Features
+    # ==========================
+
+    radius_worst: float
+    texture_worst: float
+    perimeter_worst: float
+    area_worst: float
+    smoothness_worst: float
+    compactness_worst: float
+    concavity_worst: float
+    concave_points_worst: float
+    symmetry_worst: float
+    fractal_dimension_worst: float
